f95f7770c9f11b76cd44352da584e6a1663c0a2f
Team3786Driver <mail@krrobotics.net>
2018-02-18 00:57:50 +0000

    Added LED controls

--- CleanedVision.py
+++ CleanedVision.py
@@ -11,6 +11,8 @@ import cv2
 import sys
 import pprint
 
+import LED
+
 # import ColorSensor
 
 # construct the argument parse and parse the arguments
@@ -71,6 +73,9 @@ nwt.putNumber('upper_vib', upper_vib)
 nwt.putNumber('radius', rad)
 
 nwt.putNumber('brightness', bright)
+
+# push blank color settings to network tables
+nwt.putNumberArray('LED', [0, 0, 0])
 
 # reslution
 xres = 160
@@ -145,14 +150,11 @@ cam = cv2.VideoCapture(args["camera"])
 pprinter = pprint.PrettyPrinter(indent=4)
 pprinter.pprint(cam)
 while(True):
+    color = nwt.getNumberArray('LED', (0, 0, 0))
+    LED.setColor(color)
     bright = nwt.getNumber('brightness', 100.0)
     _, image = cam.read()
     if len(image) != 0:
-        print("Got an image")
         image = imutils.resize(image, width=xres, height=yres)
         cam.set(cv2.CAP_PROP_BRIGHTNESS, bright / 100.0)
         frameUpdate(image)
-    else:
-        print("No image")
-
-
---
+++ LED.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import time
+
+from neopixel import *
+
+import signal
+import sys
+
+
+# LED strip configuration:
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP = ws.WS2811_STRIP_GRB # Strip type and colour ordering
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+strip.begin()
+
+def setColor(color):
+	for i in range(strip.numPixels()):
+		strip.setPixelColorRGB(i,int(color[0]),int(color[1]),int(color[2]))
+	strip.show()
+
