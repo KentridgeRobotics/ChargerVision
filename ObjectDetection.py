3d430298695a05db8712ce268d192a6f19350351
Team3786Driver <mail@krrobotics.net>
2018-03-20 02:08:57 +0000

    Modified methods in ObjectDetection

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -14,26 +14,31 @@ class ObjectDetector:
 
     def __init__(self, lower_hue, upper_hue):
         self.lower_hue = lower_hue
-        upper_hue = upper_hue
+        self.upper_hue = upper_hue
 
 
     #Filter Color
-      def filter_colors(img):
-        hsv = cv2.cvtColor(img, cvt.Color_BGR2HSV)
+    def filter_colors(img):
+        hsv = cv2.cvtColor(img, cvt.COLOR_BGR2HSV)
 
         mask = cv2.inRange(hsv, lower_hue, upper_hue)
+        mask = cv2.cvtColor(mask, cvt.COLOR_HSV2BGR)
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-        return; 
+        return mask; 
     
     
 
     #Find Contours
 
     def find_contours(img):
-        
-        
+        img = cv2.cvtColor(img, cvt.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(img, 127, 255, 0)
+        contours, hierarchy = cv2.findContours(thresh, 1, 2)
+
+        for x in range(0, len(contours)):
+            contours.
         return;
     
 
